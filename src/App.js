a4cc33a7c6e9068c252307a6168c06ef5ea2a3ab
Gokulkrishna Dhanvanthri <gokuld@asiapac.altair.com>
2020-05-04 21:41:48 +0530

    Implemented Shop page, collection preview & collection item component

--- src/App.js
+++ src/App.js
@@ -1,17 +1,14 @@
 import React from "react";
 import { Route, Switch } from "react-router-dom";
 import Homepage from "./pages/home/homepage";
+import Shoppage from "./pages/shop/shoppage";
 import "./App.css";
-
-const HatsPage = () => {
-  return <h1>Hats buy page!!!</h1>;
-};
 
 function App() {
   return (
     <Switch>
       <Route exact path="/" component={Homepage} />
-      <Route path="/shop/hats" component={HatsPage} />
+      <Route path="/shop" component={Shoppage} />
     </Switch>
   );
 }
