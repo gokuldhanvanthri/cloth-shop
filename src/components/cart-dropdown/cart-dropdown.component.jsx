ddab8ae069cf1a11212dd096f7513bcccdacefbd
Gokulkrishna Dhanvanthri <gokuld@asiapac.altair.com>
2020-11-16 16:49:35 +0530

    Added reselect libarary for memoization

--- src/components/cart-dropdown/cart-dropdown.component.jsx
+++ src/components/cart-dropdown/cart-dropdown.component.jsx
@@ -3,6 +3,7 @@ import { connect } from "react-redux";
 
 import CustomButtom from "../button/button";
 import CartItem from "../cart-item/cart-item.component";
+import { selectCartItems } from "../../redux/cart/cart.selecter";
 
 import "./cart-dropdown.styles.scss";
 
@@ -20,8 +21,8 @@ const Cart = ({ cartItems = [] }) => {
   );
 };
 
-const mapStateToProps = ({ cart: { cartItems } }) => ({
-  cartItems,
+const mapStateToProps = (state) => ({
+  cartItems: selectCartItems(state),
 });
 
 export default connect(mapStateToProps)(Cart);
