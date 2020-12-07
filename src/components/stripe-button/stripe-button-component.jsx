4e13d749e8a6e5574efeb056b18b310d724604cc
Gokulkrishna Dhanvanthri <gokuld@asiapac.altair.com>
2020-12-07 20:07:23 +0530

    Updating Stripe integration, favicon & title

---
+++ src/components/stripe-button/stripe-button-component.jsx
@@ -0,0 +1,30 @@
+import React from "react";
+import StripeCheckout from "react-stripe-checkout";
+
+const StripeCheckoutButton = ({ price }) => {
+  const priceForStripe = price * 100;
+  const publishableKey =
+    "pk_test_51Hvk6tI6iSFuzOMZbjyhjJ9UqVXAcMOSOF2O3DgqpAAzXa3PlZVgVRZWQf9JyIH17gwseOnC4u1hFWp1GKL6ZgOt00vDDI1pr7";
+
+  const onToken = (token) => {
+    console.log(token);
+    alert("Payment Successful");
+  };
+
+  return (
+    <StripeCheckout
+      label="Pay Now"
+      name="Cool Clothing ltd"
+      billingAddress
+      shippingAddress
+      image="https://sendeyo.com/up/d/f3eb2117da"
+      description={`Your total is $${price}`}
+      amount={priceForStripe}
+      panelLabel="Pay Now"
+      token={onToken}
+      stripeKey={publishableKey}
+    />
+  );
+};
+
+export default StripeCheckoutButton;
