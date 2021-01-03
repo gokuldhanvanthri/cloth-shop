1a1ea1aa0ecd7c70e0c990ea7debf0614abeefa8
Gokulkrishna Dhanvanthri <gokuld@asiapac.altair.com>
2021-01-03 19:50:11 +0530

    Updating code to be pwa compliant, added service workers, https, updated manifest

--- server.js
+++ server.js
@@ -14,22 +14,20 @@ const stripe = require("stripe")(process.env.STRIPE_SECRET_KEY);
 const app = express();
 // 5000 for local server
 const port = process.env.PORT || 5000;
-
-app.use(compression());
 // Process all the request's body tag and convert it to json
 app.use(bodyParser.json());
-// Enforces HTTPS connections on any incoming request. HTTP request will be redirected to an HTTPS address.
-app.use(enforce.HTTPS({ trustProtoHeader: true }));
 // Encode the url passing in and passing out - should encode characters like space/symbols
 app.use(bodyParser.urlencoded({ extended: true }));
-
 // Enables the cross origin requests to communicate b/w client (port 3000) & server (port 5000)
 app.use(cors());
 
 if (process.env.NODE_ENV === "production") {
+  // Compress the file with gzip
+  app.use(compression());
+  // Enforces HTTPS connections on any incoming request. HTTP request will be redirected to an HTTPS address.
+  app.use(enforce.HTTPS({ trustProtoHeader: true }));
   // Serve all the static files of the client's built artifacts
   app.use(express.static(path.join(__dirname, "client/build")));
-
   // Serve the client index.html for any get route which doesnt have any API coverage
   app.get("*", function (req, res) {
     res.sendFile(path.join(__dirname, "client/build", "index.html"));
